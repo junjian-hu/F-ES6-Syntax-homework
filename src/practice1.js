0bf7ef5b0ea3e431e1438c5e2eb6d8572a384752
junjian-hu <junjian.hu@thoughtworks.com>
2020-11-10 15:57:50 +0800

    homework finished

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,25 @@
-const parseData
+const parseData = (DATA) => {
+    var keys = [];
+    var output = [];
+    for(const tmp of DATA.column){
+        let { name } = tmp;
+        keys.push(name);
+    }
+
+    DATA.data.forEach(element => {
+        let obj = {};
+        if(keys.length == element.length){
+            for(let i = 0; i< keys.length;i++){
+                obj[keys[i]] = element[i];
+            }
+        }else{
+            throw new Error();
+        }
+
+        output.push(obj);
+
+    });
+    return output;
+
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,11 @@
-const inject
+const inject = (items, sections) => {
+    var lastPosition;
+    sections.forEach(element => {
+        let {content, index} = element;
+        index = lastPosition <= index ? index+=1 : index;
+        items.splice(index, 0, content);
+        lastPosition = index;
+    }); 
+    return items;
+}
 export { inject };
